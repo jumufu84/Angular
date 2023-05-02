3142f64f2a5bb70a6429ae5c85328155555c1eeb
Julio <jumufu84@gmail.com>
2023-05-02 15:50:26 +0100

    Incluimos pagina de error

--- Angular-adv/03-adminpro/src/app/pages/nopagefound/nopagefound.component.ts
+++ Angular-adv/03-adminpro/src/app/pages/nopagefound/nopagefound.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nopagefound',
   templateUrl: './nopagefound.component.html',
-  styles: [
-  ]
+  styleUrls: [ './nopagefound.component.css' ]
 })
-export class NopagefoundComponent {
+
+
+export class NopagefoundComponent{
+
+  year = new Date().getFullYear();
 
 }
