67c92859ebf625967d9f2820ee970d50b818a33e
Julio <jumufu84@gmail.com>
2023-05-21 21:53:59 +0100

    Fin sección 8

--- Angular-adv/03-adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ Angular-adv/03-adminpro/src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { Subscription, filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,6 +8,29 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy{
+
+  public titulo!: string;
+  public tituloSubs$: Subscription;
+
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.tituloSubs$ = this.getArgumentosRuta().subscribe( data => {
+                                                this.titulo = data["titulo"];
+                                                document.title = `AdminPro - ${data["titulo"]}`;
+                                              });
+  }
+
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+
+  getArgumentosRuta() {
+    return this.router.events
+    .pipe(
+      filter((event): event is ActivationEnd => event instanceof ActivationEnd),
+      filter((event:ActivationEnd) => event.snapshot.firstChild === null ),
+      map((event:ActivationEnd) => event.snapshot.data)
+    )
+  }
 
 }
